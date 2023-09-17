24b678cdbae9f0b0494f565dfa7347b3ea083d2e
adityash12 <128187345+adityash12@users.noreply.github.com>
2023-09-18 01:14:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+#btn{
+	background-color:black;
+	color:white;
+	border-radius:5px;
+	border-style:none;
+}
+#btn:hover{
+	background-color:yellow;
+	color:black;
+	border-radius:30px;
+	
+}
